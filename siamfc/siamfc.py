2d9203bc5568749320a97bc64a836423a4defebc
Martin <mb0484@student.uni-ljk.si>
2022-05-22 18:51:47 +0200

    Algorithm update

--- siamfc/siamfc.py
+++ siamfc/siamfc.py
@@ -140,7 +140,7 @@ class TrackerSiamFC(Tracker):
         
         
     def sample_random_points(self, size_of_img):
-        arr = []
+        arr = np.array([])
         x_positions = np.random.randint(10, size_of_img[0], self.num_samples).astype("float")
         y_positions = np.random.randint(10, size_of_img[1], self.num_samples).astype("float")
         
